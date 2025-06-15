3cb7d3556e4d043341ce4d91a2e7683525df0597
LeConsulat2 <wooine2@gmail.com>
2025-06-15 19:52:41 +1200

    Update page.tsx

--- app/programmes/nursing/page.tsx
+++ app/programmes/nursing/page.tsx
@@ -1,13 +1,13 @@
 // app/programmes/nursing/page.tsx
 
 import ProgressTracker from "@/components/ProgressTracker";
-import { AUT_NURSING_COURSE_REQUIREMENTS  } from "./constants";
+import { AUT_NURSING_COURSE_REQUIREMENTS, TOTAL_GRADUATION_POINTS_NURSING  } from "./constants";
 import type { ProgrammeData } from "@/programmes/types";
 
 const programmeData: ProgrammeData = {
   programmeName: "AUT Nursing Course Requirements",
   courses: AUT_NURSING_COURSE_REQUIREMENTS,
-  totalPoints: AUT_NURSING_COURSE_REQUIREMENTS.reduce((sum, c) => sum + c.points, 0),
+  totalPoints: TOTAL_GRADUATION_POINTS_NURSING,
 };
 
 export default function NursingPage() {
