55cc9ea409a0582a6e5e024b3099327d5e802511
LeConsulat2 <wooine2@gmail.com>
2025-06-16 19:37:07 +1200

    Update instrumentation.ts

--- app/instrumentation.ts
+++ app/instrumentation.ts
@@ -1,7 +1,8 @@
-// instrumentation.ts - Create this file in your project root (same level as package.json)
+// instrumentation.ts - This file should be in your PROJECT ROOT (same level as package.json)
+// NOT in the app/ folder
 export async function register() {
     if (typeof globalThis !== 'undefined' && !Promise.withResolvers) {
-      // @ts-ignore
+      // @ts-expect-error - Promise.withResolvers is not available in Node.js < 22
       Promise.withResolvers = function() {
         let resolve, reject;
         const promise = new Promise((res, rej) => {
