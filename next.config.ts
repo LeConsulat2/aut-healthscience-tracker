eb58fef559b9dd204263dc15e4be3cdfad1c5cf6
LeConsulat2 <wooine2@gmail.com>
2025-06-16 22:04:55 +1200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig = {
-  webpack: (config: any) => {
-    // Add Promise.withResolvers polyfill
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      util: require.resolve("util/"),
-      stream: require.resolve("stream-browserify"),
-      buffer: require.resolve("buffer/"),
-    };
-    return config;
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
