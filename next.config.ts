e8e79b84f46ef02b6d3e03c52adc47a21182e6ae
LeConsulat2 <wooine2@gmail.com>
2025-06-16 20:19:24 +1200

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  
+const nextConfig = {
+  webpack: (config: any) => {
+    // Add Promise.withResolvers polyfill
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      util: require.resolve("util/"),
+      stream: require.resolve("stream-browserify"),
+      buffer: require.resolve("buffer/"),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
